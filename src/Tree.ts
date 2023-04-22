a1127e652b35173b977c2249a6c9786e618ef4d2
danshil <danshil@hotmail.com>
2023-04-22 11:59:38 -0400

    Complete project

--- src/Tree.ts
+++ src/Tree.ts
@@ -14,24 +14,8 @@ export default class Tree {
   }
 
   buildTree(array: Array<number>) {
-    const sortedArray = Tree.sortArray(array);
-
-    const treeify = (
-      array: Array<number>,
-      start: number = 0,
-      end: number = array.length - 1
-    ): Node | null => {
-      if (start > end) return null;
-      const mid = Math.floor((start + end) / 2);
-      const node = new Node(array[mid]);
-
-      if (mid === Math.floor((array.length - 1) / 2)) this.root = node;
-      node.left = treeify(array, start, mid - 1);
-      node.right = treeify(array, mid + 1, end);
-      return node;
-    };
-
-    treeify(sortedArray);
+    const sortedArray = this.sortArray(array);
+    this.treeify(sortedArray);
   }
 
   insert(value: number) {
@@ -93,8 +77,124 @@ export default class Tree {
     else if (root.data > value) return this.find(value, root.left);
   }
 
-  static sortArray(array: Array<number>): Array<number> {
+  levelOrder(callback = this.returnData) {
+    const queue: Array<Node> = [this.root];
+    const ordered: Array<Node> = [];
+
+    while (queue[0]) {
+      const node = queue[0];
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      ordered.push(queue.shift());
+    }
+    return callback(ordered);
+  }
+
+  levelOrderRecursive(callback = this.returnData) {
+    const recursion = (
+      queue: Array<Node> = [this.root],
+      ordered: Array<Node> = []
+    ): Array<Node> => {
+      const node: Node = queue.shift();
+      if (!node) return ordered;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      ordered.push(node);
+      return recursion(queue, ordered);
+    };
+    return callback(recursion());
+  }
+
+  preorder(callback = this.returnData) {
+    const iter = (node = this.root, order: Array<Node> = []): Array<Node> => {
+      if (!node) return order;
+      order.push(node);
+      if (node.left) iter(node.left, order);
+      if (node.right) iter(node.right, order);
+      return order;
+    };
+    return callback(iter());
+  }
+
+  inorder(callback = this.returnData) {
+    const iter = (node = this.root, order: Array<Node> = []): Array<Node> => {
+      if (!node) return order;
+      if (node.left) iter(node.left, order);
+      order.push(node);
+      if (node.right) iter(node.right, order);
+      return order;
+    };
+    return callback(iter());
+  }
+
+  postorder(callback = this.returnData) {
+    const iter = (node = this.root, order: Array<Node> = []): Array<Node> => {
+      if (!node) return order;
+      if (node.left) iter(node.left, order);
+      if (node.right) iter(node.right, order);
+      order.push(node);
+      return order;
+    };
+    return callback(iter());
+  }
+
+  depth(value: Node, root = this.root): number {
+    if (!root || !value) return -1;
+    let counter = -1;
+    if (
+      value === root ||
+      (counter = this.depth(value, root.left)) >= 0 ||
+      (counter = this.depth(value, root.right)) >= 0
+    )
+      return counter + 1;
+    return counter;
+  }
+
+  height(node: Node): number {
+    if (!node) return 0;
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  isBalanced(): boolean {
+    const innerCheck = (node: Node = this.root): number => {
+      if (!node) return 0;
+      let leftTree = innerCheck(node.left);
+      if (leftTree === -1) return -1;
+      let rightTree = innerCheck(node.right);
+      if (rightTree === -1) return -1;
+      if (Math.abs(leftTree - rightTree) > 1) return -1;
+      return Math.max(leftTree, rightTree) + 1;
+    };
+    return innerCheck() === -1 ? false : true;
+  }
+
+  rebalance() {
+    this.treeify(this.inorder());
+  }
+
+  private returnData(nodes: Node[]): any {
+    return nodes.map((node) => node.data);
+  }
+
+  private sortArray(array: Array<number>): Array<number> {
     return [...new Set(array)].sort((a, b) => a - b);
+  }
+
+  private treeify(
+    array: Array<number>,
+    start: number = 0,
+    end: number = array.length - 1
+  ): Node | null {
+    if (start > end) return null;
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(array[mid]);
+
+    if (mid === Math.floor((array.length - 1) / 2)) this.root = node;
+    node.left = this.treeify(array, start, mid - 1);
+    node.right = this.treeify(array, mid + 1, end);
+    return node;
   }
 
   static prettyPrint(node: Node, prefix = '', isLeft = true): void {
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ import Tree from './Tree';
 const tree = new Tree();
 tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 Tree.prettyPrint(tree.root);
-console.log(tree.find(5));
+console.log(tree.depth(tree.find(8)));
+// console.log(tree.levelOrder());
+// console.log(tree.levelOrderRecursive());
+
+// const tree1 = new Tree();
+// tree1.buildTree([25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]);
+// Tree.prettyPrint(tree1.root);
+// console.log(tree1.preorder());
+// console.log(tree1.inorder());
+// console.log(tree1.postorder());
+// console.log(tree1.isBalanced());
 
 // const tree2 = new Tree(2);
 // tree2.insert(1);
@@ -14,16 +24,43 @@ console.log(tree.find(5));
 // Tree.prettyPrint(tree3.root);
 
 // const blankTree1 = new Tree();
-// blankTree1.insert(1);
+// blankTree1.insert(5);
 // blankTree1.insert(1000);
 // blankTree1.insert(2000);
+// blankTree1.insert(3000);
+// blankTree1.insert(4000);
+// blankTree1.insert(2500);
+// blankTree1.insert(2499);
+// blankTree1.insert(2498);
+// blankTree1.insert(2);
 // Tree.prettyPrint(blankTree1.root);
 
-// const randomTree = new Tree();
-// for (let i = 0; i < 200; i++) {
-//   randomTree.insert(Math.floor(Math.random() * 100));
-// }
+const log = (string: any): void => console.log(string);
+
+const randomTree = new Tree(randomNumbers());
 // Tree.prettyPrint(randomTree.root);
-// randomTree.find(33)
-// randomTree.delete(33);
-// Tree.prettyPrint(randomTree.root);
+// log(randomTree.isBalanced())
+// log(randomTree.levelOrder())
+// log(randomTree.preorder())
+// log(randomTree.postorder())
+// log(randomTree.inorder())
+for (let i = 0; i < 200; i++) {
+  randomTree.insert(Math.floor(Math.random() * 1000));
+}
+log(randomTree.height(randomTree.root));
+log(randomTree.isBalanced());
+randomTree.rebalance();
+log(randomTree.isBalanced());
+log(randomTree.height(randomTree.root));
+log(randomTree.levelOrder());
+log(randomTree.preorder());
+log(randomTree.postorder());
+log(randomTree.inorder());
+
+function randomNumbers(d = 100, n = 20) {
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(Math.floor(Math.random() * 10 * d));
+  }
+  return array;
+}
